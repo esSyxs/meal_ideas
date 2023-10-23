8d31271f5da0727f6413ad2a5f140399d1ba0316
esSyxs <e.s.cyberr@gmail.com>
2023-10-24 00:36:13 +0300

    added produce to recepies

--- food/recepies.go
+++ food/recepies.go
@@ -14,11 +14,23 @@ var (
 	recepiesMux sync.Mutex
 	aplID       autoIncrement
 	appliances  map[string]*Appliance
+	produceID   autoIncrement
+	produce     map[string]*Produce
 )
 
 func init() {
 	recepies = make(map[uint]*Recepie)
 	appliances = make(map[string]*Appliance)
+	produce = make(map[string]*Produce)
+
+	for i := 1; i <= 10; i++ {
+		name := fmt.Sprintf("Produce %d", i)
+		produce[name] = &Produce{
+			ID:       uint(produceID.id()),
+			Name:     name,
+			Recepies: nil,
+		}
+	}
 
 	for i := 1; i <= 10; i++ {
 		name := fmt.Sprintf("Appliance %d", i)
@@ -36,6 +48,7 @@ func init() {
 			Name:       fmt.Sprintf("Food %d", i),
 			Desciption: fmt.Sprintf("Super tasty %d", i),
 			Users:      nil,
+			Produces:   nil,
 			Appliances: nil,
 		}
 
@@ -49,6 +62,18 @@ func init() {
 		for i := 1; i <= 10; i++ {
 			if i == app1 || i == app2 || i == app3 {
 				recepies[id].Appliances = append(recepies[id].Appliances, appliances[fmt.Sprintf("Appliance %d", i)])
+			}
+		}
+
+		// very bad practice
+		<-time.After(time.Millisecond)
+		app4 := randInt(1, 5)
+		<-time.After(time.Millisecond)
+		app5 := randInt(6, 10)
+
+		for i := 1; i <= 10; i++ {
+			if i == app4 || i == app5 {
+				recepies[id].Produces = append(recepies[id].Produces, produce[fmt.Sprintf("Produce %d", i)])
 			}
 		}
 	}
